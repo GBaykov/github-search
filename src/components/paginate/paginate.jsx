ef76abac415391fc487d321dc5621262538087d2
GBaykov <gleb.baykov99@gmail.com>
2022-09-18 23:37:25 +0300

    feat: add page of pages in pagination

--- src/components/paginate/paginate.jsx
+++ src/components/paginate/paginate.jsx
@@ -15,12 +15,15 @@ export default function PaginatedItems({ itemsPerPage }) {
   // const [itemOffset, setItemOffset] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
+  const [endOfSet, setEndOfSet] = useState(0);
+  const [beginOfSet, setBeginOfSet] = useState(0);
 
   const { state, dispatch } = useContext(AppContext);
 
   // const isComponentMounted = hooks.useComponentDidMount();
 
   async function addRepos(userName, currentPage) {
+    setIsLoading(true);
     try {
       const repos = await getUserRepos(userName, currentPage);
       console.log(repos, 'repos in addRepos in Paginate');
@@ -31,11 +34,19 @@ export default function PaginatedItems({ itemsPerPage }) {
     }
   }
   useEffect(() => {
-    setIsLoading(true);
     addRepos(state.userName, state.currentPage);
     setPageCount(Math.ceil(state.reposLenght / itemsPerPage));
     setIsLoading(false);
   }, [state.userName, state.currentPage]);
+
+  useEffect(() => {
+    setBeginOfSet(state.currentPage * itemsPerPage - 3);
+    const remnant = (state.currentPage * itemsPerPage) % state.reposLenght;
+    console.log(remnant);
+    const endOfS =
+      remnant >= 4 ? state.currentPage * itemsPerPage : state.currentPage * itemsPerPage - remnant;
+    setEndOfSet(endOfS);
+  }, [state.userName, state.currentPage, state.user, state.repos]);
 
   // useEffect(() => {
   //   if (isComponentMounted) {
@@ -61,23 +72,34 @@ export default function PaginatedItems({ itemsPerPage }) {
   };
   if (isLoading) return <Spinner />;
 
+  function pageCountSet() {
+    return (
+      <p className="page-count">
+        {beginOfSet}-{endOfSet} of {state.reposLenght} items
+      </p>
+    );
+  }
+
   return (
     <>
       <Items repos={state.repos} />
-      <ReactPaginate
-        breakLabel="..."
-        // breakClassName="break-item"
-        // breakLinkClassName="break-link"
-        nextLabel=" next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={2}
-        marginPagesDisplayed={1}
-        pageCount={pageCount}
-        previousLabel="< previous "
-        renderOnZeroPageCount={null}
-        className="pagination"
-        activeClassName="active-page"
-      />
+      <div className="paginate-count">
+        {pageCountSet()}
+        <ReactPaginate
+          breakLabel="..."
+          // breakClassName="break-item"
+          // breakLinkClassName="break-link"
+          nextLabel=" next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={2}
+          marginPagesDisplayed={1}
+          pageCount={pageCount}
+          previousLabel="< previous "
+          renderOnZeroPageCount={null}
+          className="pagination"
+          activeClassName="active-page"
+        />
+      </div>
     </>
   );
 }
